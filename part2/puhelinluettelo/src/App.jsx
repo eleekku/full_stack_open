71cd12e8add28aaa01fe02f54790a321e7e3611d
Elias Salmela <144835604+eleekku@users.noreply.github.com>
2025-02-20 22:39:16 +0200

    phonebook add number

--- part2/puhelinluettelo/src/App.jsx
+++ part2/puhelinluettelo/src/App.jsx
@@ -2,26 +2,56 @@ import { useState } from 'react'
 
 const Persons = ({ person }) => {
   return (
-    <li>{person.name}</li>
+    <li>{person.name} {person.number}</li>
   )
 }
 
-const handleNewName = (event) => {
+const handleNewNumber = (event, setNewNumber) => {
+  setNewNumber(event.target.value)
+}
+
+const handleNewName = (event, setNewName) => {
   setNewName(event.target.value)
+}
+
+const addPerson = (event, persons, setPersons, newName, setNewName, newNumber, setNewNumber) => {
+  event.preventDefault()
+  const newPerson = { name: newName, number: newNumber }
+  if (newPerson.name === '') {
+    alert('Name cannot be empty')
+    return
+  }
+  if (persons.map(person => person.name).includes(newPerson.name)) {
+    alert(`${newPerson.name} is already added to phonebook`)
+    return
+  }
+  if (newPerson.number === '') {
+    alert('Number cannot be empty')
+    return
+  }
+  if (newPerson.number)
+  setPersons(persons.concat(newPerson))
+  setNewName('')
+  setNewNumber('')
 }
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas',
+      number: '040-1234567' }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={(event) => addPerson(event, persons, setPersons, newName, setNewName, newNumber, setNewNumber)}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={(event) => handleNewName(event, setNewName)} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={(event) => handleNewNumber(event, setNewNumber)}/>
         </div>
         <div>
           <button type="submit">add</button>
@@ -30,13 +60,10 @@ const App = () => {
       <h2>Numbers</h2>
       <div>debug: {newName}</div>
       <ul>
-        {persons.map(person => 
-          <Persons key={person.name} person={person} />
-        )}
+        {persons.map(person => <Persons key={person.name} person={person} />)}
       </ul>
     </div>
   )
-
 }
 
 export default App
