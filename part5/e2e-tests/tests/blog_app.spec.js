61e0efcf28c8ca3304e75c0ddfe70be260d32659
Elias Salmela <144835604+eleekku@users.noreply.github.com>
2026-04-07 20:17:07 +0300

    5.17

---
+++ part5/e2e-tests/tests/blog_app.spec.js
@@ -0,0 +1,18 @@
+const { test, expect, beforeEach, describe } = require('@playwright/test')
+
+describe('Blog app', () => {
+  beforeEach(async ({ page }) => {
+    await page.goto('http://localhost:5173')
+  })
+
+  test('Login form is shown', async ({ page }) => {
+    const loginButton = page.getByRole('button', { name: 'log in' })
+    await expect(loginButton).toBeVisible()
+
+    await loginButton.click()
+
+    await expect(page.getByText('username')).toBeVisible()
+    await expect(page.getByText('password')).toBeVisible()
+    await expect(page.getByRole('button', { name: 'login' })).toBeVisible()
+  })
+})
